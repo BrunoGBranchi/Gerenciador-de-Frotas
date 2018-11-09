108b4b6a851a92d3a74bcfcc7b94b45a270704be
Bruno Ghilardi Branchi <38675135+BrunoGBranchi@users.noreply.github.com>
2018-11-09 19:51:21 -0200

    Atualizado Script Banco de Dados

--- Gerenciador-de-Frotas/ControleFrota/Gerenciador de frota.sql
+++ Gerenciador-de-Frotas/ControleFrota/Gerenciador de frota.sql
@@ -3,6 +3,9 @@ use Frotas;
 
 drop table empresa;
 
+insert into empresa (razaoSocial, nomeFantasia, CNPJ, endereco, numero, bairro, cep, municipio, uf) values
+('MGBTurismo', 'Topsul Viagens','12300','Rua Amazonas','253','Dos Esportes','89820000','Xanxere', 'SC');
+
 create table empresa(
 codigo bigint(20) not null auto_increment primary key,
 razaoSocial varchar(100),
@@ -16,7 +19,9 @@ municipio varchar(100),
 uf varchar(100)
 );
 
+update  empresa set razaoSocial = 'MGB',nomeFantasia = 'TPV',cnpj = '561465', endereco = 'AMz', numero = '253', bairro = 'esp',cep = '89',municipio = 'xxe',uf = 'SC' where codigo = 5;
 
+select * from empresa;
 
 create table funcionarios(
 codigo bigint(20) not null auto_increment primary key,
@@ -51,7 +56,7 @@ aplicacao varchar(100),
 datacad datetime  
 );
 
-	
+	drop table veiculos;
 create table veiculos(
 codigo bigint(20) not null auto_increment primary key,
 renavam bigint(20),
@@ -60,10 +65,15 @@ modelo varchar(100),
 placa varchar(100),
 motor varchar(100),
 chassi varchar(100),
-categoria varchar(100)
+categoria varchar(100),
+datacad datetime
 );
-    
-    drop table usuarios;
+
+select * from veiculos;
+delete from veiculos where codigo = 3;
+insert into veiculos (renavam, marca, modelo, placa, motor, chassi, categoria) values(?,?,?,?,?,?,?);
+
+drop table usuarios;
 create table usuarios(
 codigo bigint(20) not null auto_increment primary key,
 nome varchar(100),
@@ -71,12 +81,9 @@ senha varchar(100),
 usuario varchar(100),
 datacad datetime
 );   
-use frotas;
+
 insert into usuarios (nome, senha, usuario, datacad) values ('Administrador', 'admin', 'admin', now());
 insert into usuarios (nome, senha, usuario, datacad) values('Bruno', 'bruno', '09021999', now());
 insert into usuarios values('Administrador', 'admin', 'admin', now());
-<<<<<<< HEAD
 select * from usuarios;
-=======
-select * from usuarios;
->>>>>>> branch 'master' of https://github.com/BrunoGBranchi/Gerenciador-de-Frotas
+select * from empresa ;
